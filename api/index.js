d5f6af3eb19b55ab55f7ad1c6b9b55d825367c20
DHAMOTHARANJ <dhamothranj@outlook.com>
2023-11-14 12:25:35 +0530

    changed

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const salt = bcrypt.genSaltSync(10);
 const secret = 'qergbkjavauiohvb'
 
 
-app.use(cors({credentials: true, origin : 'http://localhost:3000'}));
+app.use(cors({credentials: true, origin : 'https://unrivaled-meerkat-f3a0b8.netlify.app'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'))
